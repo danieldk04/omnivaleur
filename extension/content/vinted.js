115d66dedf4b0483cfdfba60d06a29ae0f7ba9e3
Daniel de Koning <danieldekoning66@gmail.com>
2026-08-10 15:00:39 +0200

    auto: update vinted.js

--- extension/content/vinted.js
+++ extension/content/vinted.js
@@ -1533,11 +1533,6 @@
     )?.el || null;
   }
 
-  // Wat er bij de kleurstap gebeurde, in gewone taal. Belandt in het achtergrond-
-  // logboek én in de foutmelding op het dashboard, zodat een mislukte kleur na te
-  // lezen is zonder de console van het tabblad open te hoeven hebben.
-  let kleurDiagnose = "kleurstap is niet gedraaid";
-
   async function fillColourVinted(item) {
     const colours = parseColours(item);
     if (!colours.length) {
