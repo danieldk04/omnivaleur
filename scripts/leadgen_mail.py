066d680ed20f32aa0f708b9e1ce9862718a2c893
Daniel de Koning <danieldekoning66@gmail.com>
2026-08-11 13:26:40 +0200

    auto: update leadgen_mail.py

--- scripts/leadgen_mail.py
+++ scripts/leadgen_mail.py
@@ -205,6 +205,9 @@ week kwijt aan het overzetten van advertenties naar andere platforms?
 Is het antwoord "te veel", dan stuur ik {jou} alsnog graag die video van een
 minuut. {jij_ziet} in één oogopslag wat {bedrijf} daarvan overneemt.
 
+{jij_kunt} het daarna 7 dagen gratis proberen, zonder dat {jij} ergens aan
+vastzit.
+
 {ondertekening}"""
 
 MAIL3 = """{aanhef} {naam},
